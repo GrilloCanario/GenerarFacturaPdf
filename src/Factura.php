5ae401a58bf586cb42650cd65ebd964e2c275353
PedroBarrera <pedrodbarrerafdez@gmail.com>
2025-10-21 01:50:56 +0000

    Prototipo de constructor Factura

--- src/Factura.php
+++ src/Factura.php
@@ -1,2 +1,20 @@
 <?php
 
+
+class Factura {
+    public function __construct(
+        public string $nombre,
+        public string $direccion,
+        public int $num_factura,
+        public int $fecha,
+        public array $lista_conceptos = []
+    ) {
+    }    
+    public function agregarConcepto(string $descripcion, int $cantidad, float $precioUnitario){
+
+        }
+
+    public function calcularTotal() {
+        
+    }
+}
